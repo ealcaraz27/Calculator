e997148f331b6bf511868cc6ec3a621de70648fc
ealcaraz27 <eric.alcaraz27@gmail.com>
2022-12-11 23:58:13 -0500

    This is the first skeleton of the code to be made

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event;
+
+public class Calculator implements ActionListener{
+
+}
+public static void main(String args[]){
+    Calculator calc = new Calculator();
+}
